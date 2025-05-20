515d1827bbe587ef706c71c2106badc1521abfdd
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-20 21:38:57 +0000

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -39,7 +39,7 @@ const About = () => {
             />
             <AnimatedBody
               text={
-                "Outside coding, I’m a Digital Creator crafting engaging content, a Real Madrid an fan, and a music enthusiast vibing to RnB, UK Drill, and Chill Rap."
+                "Outside coding, I’m a Digital Creator crafting engaging content, a Real Madrid and fan, and a music enthusiast vibing to RnB, UK Drill, and Chill Rap."
               }
             />
             <AnimatedBody
