90b8fae05d7c2b733ef816bfed3a6fbc7a5c4e96
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-20 21:32:28 +0000

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -39,7 +39,7 @@ const About = () => {
             />
             <AnimatedBody
               text={
-                "Outside coding, I’m a Digital Creator crafting engaging content, a ManchetUied fan, and a music enthusiast vibing to RnB, UK Drill, and Chill Rap."
+                "Outside coding, I’m a Digital Creator crafting engaging content, a ManchtUied fan, and a music enthusiast vibing to RnB, UK Drill, and Chill Rap."
               }
             />
             <AnimatedBody
