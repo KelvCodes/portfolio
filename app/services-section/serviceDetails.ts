e623dda988daf74dc00155b28ad501a78ead1c7d
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-28 15:06:22 -0700

    Update serviceDetails.ts

--- app/services-section/serviceDetails.ts
+++ app/services-section/serviceDetails.ts
@@ -16,4 +16,5 @@ export const serviceDetails: serviceProps[] = [
      name: "Blockchain Development",
     description: "Building secure and transparent decentralized applications and smart contracts using Web3.js, ethers.js, and Solidity for the future of the internet.",
      icon: "FaEthereum",
+     index: 1,
       
