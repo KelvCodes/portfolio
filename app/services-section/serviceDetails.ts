886a578aa88f576d33bdfa7f084f98736f5d54b7
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-28 15:08:58 -0700

    Update serviceDetails.ts

--- app/services-section/serviceDetails.ts
+++ app/services-section/serviceDetails.ts
@@ -25,5 +25,27 @@ export const serviceDetails: serviceProps[] = [
      icon: "FaBrain",
     index: 2,
   },
+   {
+    name: "UI/UX Design",
+    description:
+      "Designing beautiful, intuitive, and user-centric interfaces with tools like Figma and Framer to ensure captivating and smooth user journeys.",
+    icon: "FaPaintBrush",
+    index: 3,
+  },
+  {
+    name: "Digital Content Creation",
+    description:
+      "Producing impactful visual content—from videos to graphics—for social media and marketing using Adobe Premiere Pro, Canva, and Photoshop.",
+    icon: "FaVideo",
+    index: 4,
+  },
+  {
+    name: "Technical Consulting",
+    description:
+      "Offering strategic guidance in web development, blockchain, and AI integration to help teams innovate, scale, and achieve their tech goals.",
+    icon: "FaComments",
+    index: 5,
+  },
+];
   
       
