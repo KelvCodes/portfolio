a3d1b036223d27843b93a21bbbedc0f29adfae04
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-29 15:02:15 -0700

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,7 +1,4 @@
 
-            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
-          }
-          wordSpace={"mr-[14px]"}
           charSpace={"mr-[0.001em]"}
         />
 
