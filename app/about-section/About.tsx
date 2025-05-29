f5e936fdb38d494f535da4b71ed461dd40815ce3
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-29 15:08:48 -0700

    Update About.tsx\

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,1 +1,112 @@
+import SongCarousel from "./SongCarousel";
+import "../animations/animate.css";
+import AnimatedBody from "../animations/AnimatedBody";
+import AnimatedTitle from "../animations/AnimatedTitle";
 
+const About = () => {
+  return (
+    <section
+      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56"
+      id="about"
+    >
+      <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
+        <AnimatedTitle
+          text={"CRAFTING DIGITAL EXPERIENCES WITH AI & WEB3"}
+          className={
+            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+          }
+          wordSpace={"mr-[14px]"}
+          charSpace={"mr-[0.001em]"}
+        />
+
+        <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
+          <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16 lg:max-w-[90%] lg:text-[24px]">
+            <AnimatedBody
+              text={
+                "I’m a Frontend Engineer focused on building high-performance web applications for SaaS and startups, with expertise in Blockchain and Machine Learning to deliver cutting-edge solutions."
+              }
+            />
+            <AnimatedBody
+              text={
+                "My passion is creating seamless user experiences—whether designing intuitive interfaces, developing decentralized apps with Blockchain, or integrating AI-driven features. I love pushing the boundaries of technology."
+              }
+              className={"hidden"}
+            />
+            <AnimatedBody
+              text={
+                "As a tech community leader, I’ve led workshops on web development, Blockchain, and ML through Google Developer Student Clubs and Microsoft Learn Student Ambassadors, helping others unlock their potential."
+              }
+            />
+            <AnimatedBody
+              text={
+                "Beyond coding, I’m a Digital Creator (on YouTube), a Real Madrid and Arsenal fan, and a music lover—constantly vibing to RnB, UK Drill, and Chill Rap."
+              }
+            />
+            <AnimatedBody
+              text={
+                "I'm currently working on exciting projects I can’t wait to share. Always open to new opportunities and collaborations—let’s build something amazing together."
+              }
+            />
+          </div>
+
+          <div className="mb-24 flex w-[100%] flex-col gap-4 text-[18px] font-normal leading-relaxed tracking-wide text-[#e4ded7]/80 sm:mb-32 md:mb-40 md:gap-6 md:text-[16px] md:leading-normal lg:mt-0 lg:mb-16 lg:max-w-[30%] lg:text-[18px]">
+            <div className="flex flex-col gap-4 md:gap-3">
+              <AnimatedTitle
+                text={"Frontend & Blockchain Stack"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "JavaScript (ES6+), React, Next.js, TypeScript, Web3.js, ethers.js, Solidity, TensorFlow.js, Prismic CMS, Redux, Testing (RTL/Vitetest), Git/GitHub, NextAuth, Formik."
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"UI & Animation Tools"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "CSS3/SCSS, Tailwind CSS, Material UI, Framer Motion, GSAP, Bootstrap, Chart.js."
+                }
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <AnimatedTitle
+                text={"Design & Content Tools"}
+                className={
+                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
+                }
+                wordSpace={"mr-[0.25em]"}
+                charSpace={"mr-[0.01em]"}
+              />
+              <AnimatedBody
+                text={
+                  "Figma, Framer, Adobe XD/Photoshop/Premiere Pro, ProtoPie, Canva, UX Research, Prototyping."
+                }
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
+          <SongCarousel />
+          <AnimatedBody
+            text="Here are a few songs I recommend if you need fresh tunes :)"
+            className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px]"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
