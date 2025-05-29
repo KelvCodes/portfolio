c209ab7e7774a3a1ac5620786f89e2077cd01d9d
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-29 15:07:47 -0700

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,7 +1,5 @@
 
-          <AnimatedBody
-            text="A few songs I can recommend if you're looking for some fresh tunes :)"
-            className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px]"
+          <Ani="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px]"
           />
         </div>
       </div>
