a4f67d7f2529b20ccff62d384ed82b9d105ba19a
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-29 15:02:01 -0700

    Update About.tsx

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,5 +1,4 @@
-   <An
-          text={"I BUILD BEAUTIFUL BRANDS, POWERFUL WEBSITES, AND ENGAGING CONTENT — POWERED BY AI & WEB3."}
+L WEBSITES, AND ENGAGING CONTENT — POWERED BY AI & WEB3."}
           className={
             "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
           }
