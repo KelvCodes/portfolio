a22194d683b51c3706ffe92c1b085cb6540d70ff
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-30 15:10:09 -0700

    Update projectDetails.ts

--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -78,8 +78,7 @@ export const devProjects = [
   },
 
   
-  //  
-riThis is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
+rm for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
   //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
   //   github: "https://github.com/msp-nigeria/mlsc.ng-frontend",
   //   demo: "",
