dcb58b49b6a958a9beeefad9d58d26ce26159d63
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-01 14:02:45 -0700

    Update reviewDetails.ts

--- app/reviews-section/reviewDetails.ts
+++ app/reviews-section/reviewDetails.ts
@@ -28,7 +28,7 @@ export const reviewDetails: reviewProps[] = [
     company: "Google",
     profileImg: emma,
     testimonial:
-      "Kelvin was an exceptional student in my Blcain course. His smart contract project using Solidity showcased deep understanding and creativity, earning top marks.",
+      "Kelvin was an exceptional student in my Blcan course. His smart contract project using Solidity showcased deep understanding and creativity, earning top marks.",
     index: 1,
   },
   {
