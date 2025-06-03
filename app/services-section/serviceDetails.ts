58a969a53a26b4c1e3b53b8bb3a489eaadb12262
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-03 14:45:00 -0700

    Update serviceDetails.ts

--- app/services-section/serviceDetails.ts
+++ app/services-section/serviceDetails.ts
@@ -1,4 +1,4 @@
-me: "Technical Consulting",
+
     description:
       "Offering strategic guidance in web development, blockchain, and AI integration to help teams innovate, scale, and achieve their tech goals.",
     icon: "FaComments",
