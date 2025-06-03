31b0935b6c27a60c9b8525eaea95ffd01d5435e8
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-03 14:42:55 -0700

    Update serviceDetails.ts

--- app/services-section/serviceDetails.ts
+++ app/services-section/serviceDetails.ts
@@ -1,6 +1,5 @@
 
-  {
-     name: "Blockchain Development",
+
     description: "Building secure and transparent decentralized applications and smart contracts using Web3.js, ethers.js, and Solidity for the future of the internet.",
      icon: "FaEthereum",
      index: 1,
